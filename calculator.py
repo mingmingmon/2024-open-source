1405f1b108fc30666f1942706ef4c4dc59d9ec46
mingmingmon <mingmingmon@naver.com>
2024-09-26 15:23:23 +0900

    계산기의 더하기, 빼기 기능 구현

---
+++ calculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("첫 번째 정수를 입력하세요: "))
+num2 = int(input("두 번째 정수를 입력하세요: "))
+
+sum_result = num1 + num2
+sub_result = num1 - num2
+
+print(f"{num1} + {num2} = {sum_result}")
+print(f"{num1} - {num2} = {sub_result}")
