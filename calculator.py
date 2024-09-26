54c4b1ef22634c41223b262b6f556cab58fdc3a9
mingmingmon <mingmingmon@naver.com>
2024-09-26 15:27:44 +0900

    계산기의 곱셈, 나눗셈 기능 구현

--- calculator.py
+++ calculator.py
@@ -3,6 +3,14 @@ num2 = int(input("두 번째 정수를 입력하세요: "))
 
 sum_result = num1 + num2
 sub_result = num1 - num2
+mul_result = num1 * num2
+
+if num2 != 0:
+    div_result = num1 / num2
+    print(f"{num1} / {num2} = {div_result}")
+else:
+    print("0으로 나눌 수 없습니다.")
 
 print(f"{num1} + {num2} = {sum_result}")
 print(f"{num1} - {num2} = {sub_result}")
+print(f"{num1} * {num2} = {mul_result}")
